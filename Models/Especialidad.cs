101a9b04cbfcedad7cade0dd38000304c841d5a4
Luis-Diego-Galvez <luis.galvez1@catolica.edu.sv>
2022-09-18 19:23:08 -0300

    Modelos Objetos 01
    Clases y objetos

---
+++ Models/Especialidad.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asistente_Hospitalario_de_Pacientes_y_Cirugías.Models
+{
+    public class Especialidad
+    {
+        //ATRIBUTES
+        private string CodigoEspecialidad;
+        private string NombreEspecialidad;
+
+        public Especialidad(string codigoEspecialidad, string nombreEspecialidad)
+        {
+            CodigoEspecialidad = codigoEspecialidad;
+            NombreEspecialidad = nombreEspecialidad;
+        }
+        public Especialidad() { }
+
+        public string Codigo { get => this.CodigoEspecialidad; set => this.CodigoEspecialidad = value;  }
+        public string Nombre { get => this.NombreEspecialidad; set => this.NombreEspecialidad = value;  }
+    }
+}
