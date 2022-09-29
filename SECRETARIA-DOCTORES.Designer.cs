a0d26a312fd94c9bfc87ab285cd3d05139ce53fe
gekrranza <91350456+gekrranza@users.noreply.github.com>
2022-09-29 15:42:12 -0600

    Interfaces Secretaria - Gerardo

---
+++ SECRETARIA-DOCTORES.Designer.cs
@@ -0,0 +1,362 @@
+﻿
+namespace Asistente_Hospitalario_de_Pacientes_y_Cirugías
+{
+	partial class SECRETARIA_DOCTORES
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SECRETARIA_DOCTORES));
+			System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+			System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+			System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.label3 = new System.Windows.Forms.Label();
+			this.btnDoctores = new System.Windows.Forms.Button();
+			this.btnSalir = new System.Windows.Forms.Button();
+			this.btnCirugias = new System.Windows.Forms.Button();
+			this.btnAltas = new System.Windows.Forms.Button();
+			this.btnPacientes = new System.Windows.Forms.Button();
+			this.roundPicture1 = new Asistente_Hospitalario_de_Pacientes_y_Cirugías.RoundPicture();
+			this.label6 = new System.Windows.Forms.Label();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.Nombres = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			((System.ComponentModel.ISupportInitialize)(this.roundPicture1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.Color.Transparent;
+			this.panel1.Location = new System.Drawing.Point(2, 3);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(2052, 102);
+			this.panel1.TabIndex = 1;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.BackColor = System.Drawing.Color.Transparent;
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.ForeColor = System.Drawing.Color.White;
+			this.label3.Location = new System.Drawing.Point(89, 440);
+			this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(168, 37);
+			this.label3.TabIndex = 66;
+			this.label3.Text = "USUARIO";
+			// 
+			// btnDoctores
+			// 
+			this.btnDoctores.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(91)))), ((int)(((byte)(196)))), ((int)(((byte)(255)))));
+			this.btnDoctores.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+			this.btnDoctores.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btnDoctores.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnDoctores.ForeColor = System.Drawing.Color.White;
+			this.btnDoctores.Location = new System.Drawing.Point(95, 681);
+			this.btnDoctores.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.btnDoctores.Name = "btnDoctores";
+			this.btnDoctores.Size = new System.Drawing.Size(184, 57);
+			this.btnDoctores.TabIndex = 65;
+			this.btnDoctores.Text = "Doctores";
+			this.btnDoctores.UseVisualStyleBackColor = false;
+			// 
+			// btnSalir
+			// 
+			this.btnSalir.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(91)))), ((int)(((byte)(196)))), ((int)(((byte)(255)))));
+			this.btnSalir.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+			this.btnSalir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btnSalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnSalir.ForeColor = System.Drawing.Color.White;
+			this.btnSalir.Location = new System.Drawing.Point(95, 860);
+			this.btnSalir.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.btnSalir.Name = "btnSalir";
+			this.btnSalir.Size = new System.Drawing.Size(184, 57);
+			this.btnSalir.TabIndex = 64;
+			this.btnSalir.Text = "Salir";
+			this.btnSalir.UseVisualStyleBackColor = false;
+			// 
+			// btnCirugias
+			// 
+			this.btnCirugias.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(91)))), ((int)(((byte)(196)))), ((int)(((byte)(255)))));
+			this.btnCirugias.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+			this.btnCirugias.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btnCirugias.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnCirugias.ForeColor = System.Drawing.Color.White;
+			this.btnCirugias.Location = new System.Drawing.Point(95, 768);
+			this.btnCirugias.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.btnCirugias.Name = "btnCirugias";
+			this.btnCirugias.Size = new System.Drawing.Size(184, 57);
+			this.btnCirugias.TabIndex = 63;
+			this.btnCirugias.Text = "Cirugías";
+			this.btnCirugias.UseVisualStyleBackColor = false;
+			// 
+			// btnAltas
+			// 
+			this.btnAltas.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(91)))), ((int)(((byte)(196)))), ((int)(((byte)(255)))));
+			this.btnAltas.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+			this.btnAltas.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btnAltas.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnAltas.ForeColor = System.Drawing.Color.White;
+			this.btnAltas.Location = new System.Drawing.Point(95, 594);
+			this.btnAltas.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.btnAltas.Name = "btnAltas";
+			this.btnAltas.Size = new System.Drawing.Size(184, 57);
+			this.btnAltas.TabIndex = 62;
+			this.btnAltas.Text = "Altas";
+			this.btnAltas.UseVisualStyleBackColor = false;
+			// 
+			// btnPacientes
+			// 
+			this.btnPacientes.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(91)))), ((int)(((byte)(196)))), ((int)(((byte)(255)))));
+			this.btnPacientes.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+			this.btnPacientes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btnPacientes.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnPacientes.ForeColor = System.Drawing.Color.White;
+			this.btnPacientes.Location = new System.Drawing.Point(95, 512);
+			this.btnPacientes.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.btnPacientes.Name = "btnPacientes";
+			this.btnPacientes.Size = new System.Drawing.Size(184, 57);
+			this.btnPacientes.TabIndex = 61;
+			this.btnPacientes.Text = "Pacientes";
+			this.btnPacientes.UseVisualStyleBackColor = false;
+			// 
+			// roundPicture1
+			// 
+			this.roundPicture1.Image = ((System.Drawing.Image)(resources.GetObject("roundPicture1.Image")));
+			this.roundPicture1.Location = new System.Drawing.Point(41, 132);
+			this.roundPicture1.Name = "roundPicture1";
+			this.roundPicture1.Size = new System.Drawing.Size(276, 271);
+			this.roundPicture1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.roundPicture1.TabIndex = 60;
+			this.roundPicture1.TabStop = false;
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.BackColor = System.Drawing.Color.Transparent;
+			this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label6.ForeColor = System.Drawing.Color.White;
+			this.label6.Location = new System.Drawing.Point(1209, 231);
+			this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(193, 33);
+			this.label6.TabIndex = 90;
+			this.label6.Text = "Especialidad";
+			// 
+			// textBox5
+			// 
+			this.textBox5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(91)))), ((int)(((byte)(196)))), ((int)(((byte)(255)))));
+			this.textBox5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox5.Location = new System.Drawing.Point(1025, 231);
+			this.textBox5.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.textBox5.Name = "textBox5";
+			this.textBox5.Size = new System.Drawing.Size(176, 35);
+			this.textBox5.TabIndex = 89;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.BackColor = System.Drawing.Color.Transparent;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.ForeColor = System.Drawing.Color.White;
+			this.label2.Location = new System.Drawing.Point(909, 233);
+			this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(86, 33);
+			this.label2.TabIndex = 84;
+			this.label2.Text = "Edad";
+			// 
+			// textBox2
+			// 
+			this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(91)))), ((int)(((byte)(196)))), ((int)(((byte)(255)))));
+			this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox2.Location = new System.Drawing.Point(814, 233);
+			this.textBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(87, 35);
+			this.textBox2.TabIndex = 83;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.BackColor = System.Drawing.Color.Transparent;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.ForeColor = System.Drawing.Color.White;
+			this.label1.Location = new System.Drawing.Point(668, 233);
+			this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(125, 33);
+			this.label1.TabIndex = 82;
+			this.label1.Text = "Nombre";
+			// 
+			// textBox1
+			// 
+			this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(91)))), ((int)(((byte)(196)))), ((int)(((byte)(255)))));
+			this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox1.Location = new System.Drawing.Point(343, 235);
+			this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(317, 35);
+			this.textBox1.TabIndex = 81;
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.AllowUserToDeleteRows = false;
+			dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+			dataGridViewCellStyle1.ForeColor = System.Drawing.Color.White;
+			dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+			dataGridViewCellStyle1.SelectionForeColor = System.Drawing.Color.White;
+			this.dataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+			dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+			dataGridViewCellStyle2.BackColor = System.Drawing.Color.Teal;
+			dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.Window;
+			dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.Teal;
+			dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.ControlLightLight;
+			dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+			this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Nombres,
+            this.Column1,
+            this.Column6,
+            this.Column3,
+            this.Column7});
+			this.dataGridView1.EnableHeadersVisualStyles = false;
+			this.dataGridView1.GridColor = System.Drawing.SystemColors.ButtonFace;
+			this.dataGridView1.Location = new System.Drawing.Point(343, 356);
+			this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.RowHeadersVisible = false;
+			this.dataGridView1.RowHeadersWidth = 62;
+			dataGridViewCellStyle3.BackColor = System.Drawing.Color.Transparent;
+			this.dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle3;
+			this.dataGridView1.Size = new System.Drawing.Size(826, 561);
+			this.dataGridView1.TabIndex = 91;
+			// 
+			// Nombres
+			// 
+			this.Nombres.HeaderText = "Nombres";
+			this.Nombres.MinimumWidth = 20;
+			this.Nombres.Name = "Nombres";
+			this.Nombres.Width = 190;
+			// 
+			// Column1
+			// 
+			this.Column1.HeaderText = "Apellidos";
+			this.Column1.MinimumWidth = 8;
+			this.Column1.Name = "Column1";
+			this.Column1.Width = 190;
+			// 
+			// Column6
+			// 
+			this.Column6.HeaderText = "Edad";
+			this.Column6.MinimumWidth = 8;
+			this.Column6.Name = "Column6";
+			this.Column6.Width = 150;
+			// 
+			// Column3
+			// 
+			this.Column3.HeaderText = "Especialidad";
+			this.Column3.MinimumWidth = 8;
+			this.Column3.Name = "Column3";
+			this.Column3.Width = 140;
+			// 
+			// Column7
+			// 
+			this.Column7.HeaderText = "N° Carnet";
+			this.Column7.MinimumWidth = 8;
+			this.Column7.Name = "Column7";
+			this.Column7.Width = 150;
+			// 
+			// SECRETARIA_DOCTORES
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+			this.ClientSize = new System.Drawing.Size(1456, 1001);
+			this.Controls.Add(this.dataGridView1);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.textBox5);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.btnDoctores);
+			this.Controls.Add(this.btnSalir);
+			this.Controls.Add(this.btnCirugias);
+			this.Controls.Add(this.btnAltas);
+			this.Controls.Add(this.btnPacientes);
+			this.Controls.Add(this.roundPicture1);
+			this.Controls.Add(this.panel1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Name = "SECRETARIA_DOCTORES";
+			this.Text = "SECRETARIA_DOCTORES";
+			((System.ComponentModel.ISupportInitialize)(this.roundPicture1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Button btnDoctores;
+		private System.Windows.Forms.Button btnSalir;
+		private System.Windows.Forms.Button btnCirugias;
+		private System.Windows.Forms.Button btnAltas;
+		private System.Windows.Forms.Button btnPacientes;
+		private RoundPicture roundPicture1;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Nombres;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+	}
+}
